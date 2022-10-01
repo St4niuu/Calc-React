ba4d47ba65cc71cfb05e3f5775ca8773be11c90b
Staniuu <staniuu123@gmail.com>
2022-10-01 14:48:56 +0200

    Init and preparation

---
+++ README.md
@@ -0,0 +1,1 @@
+# Calc using React, Vite, Styled-Components project.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+
+  return null
+}
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />)
+
+
